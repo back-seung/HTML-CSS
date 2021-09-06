bf61550ce05c07ff8e2c046b5f05460fe7111548
DESKTOP-RVNGDSL\seung <skud113@naver.com>
2021-09-07 00:22:30 +0900

    현재 위치의 날씨 기능 추가
    OPEN API를 스스로 적용해보기 위해 추가 해봤습니다.
    현재 사용자의 위치(위도, 경도)를 받고, 날씨(맑음, 비 등)/ 온도 18.3, 현재 위치(ㅇㅇ동)를 표시합니다.

---
+++ getWeather.js
@@ -0,0 +1,21 @@
+const API_KEY = "1d78806a7d1232a4579ae1994546461f";
+
+function onGeoOk(position) {
+  const lat = position.coords.latitude;
+  const lng = position.coords.longitude;
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}&units=metric`;
+  fetch(url)
+    .then((Response) => Response.json())
+    .then((data) => {
+      const weather = document.querySelector("#weather span:first-child");
+      const city = document.querySelector("#weather span:last-child");
+      const name = data.name;
+      city.innerText = name;
+      weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
+    });
+}
+function onGeoError() {
+  alert("Can't find you. No weather for you");
+}
+
+navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError);
